c0a69fc38b0206d822b36a5448ae2ff90e9bf570
yassinesebbar <38429359+yassinesebbar@users.noreply.github.com>
2021-03-02 22:28:07 +0100

    1.1

--- Bioscoop/Models/Movie.cs
+++ Bioscoop/Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bioscoop.Models
 {
@@ -7,24 +8,39 @@ namespace Bioscoop.Models
     {
         public int ID {get; set;}
         
+        [Display(Name = "Releasedatum")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate {get; set;}
 
+        [Display(Name = "Afbeelding")]
         public String ImageCover {get; set;}
 
+
+        [Display(Name = "Titel")]
         public string Title {get; set;}
 
+
+        [Display(Name = "Omschrijving")]
         public string Description {get; set;}
 
+        [Display(Name = "Genre")]
         public string Genre {get; set;}
 
+        [Display(Name = "Kijkwijzer")]
         public string RatingGuide {get; set;}
 
+        [Display(Name = "Regisseur")]
         public string director {get; set;} 
+
+        [Display(Name = "Cast")]
 
         public string Cast {get; set;}
 
+        [Display(Name = "Duur")]
         public int DurationMin {get; set;}
+
+        [Display(Name = "Prijs")]
         public decimal Price {get; set;}
+
     }
 }
