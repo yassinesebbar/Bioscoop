c63d60fef664dd4acdb6ae4411159353e5123bca
yassinesebbar <38429359+yassinesebbar@users.noreply.github.com>
2021-03-10 19:30:37 +0100

    Finishing sprint 1

--- Bioscoop/Startup.cs
+++ Bioscoop/Startup.cs
@@ -52,9 +52,7 @@ namespace Bioscoop
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseDeveloperExceptionPage();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
